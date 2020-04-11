c85ddb1a48d6dda2c275e905aa2e167bfc222c4e
Mateusz Majewski <majewski.mateusz000@gmail.com>
2020-04-11 13:30:35 +0200

    base "customers purchase sort find" class

--- src/zad2/CustomersPurchaseSortFind.java
+++ src/zad2/CustomersPurchaseSortFind.java
@@ -7,5 +7,21 @@
 package zad2;
 
 
+import java.io.File;
+
 public class CustomersPurchaseSortFind {
+    
+    private File file;
+    
+    void readFile(String path){
+        file = new File(path);
+    }
+    void showSortedBy(String sortBy){
+    
+    }
+    void showPurchaseFor(String id){
+    
+    }
+    
+    
 }
