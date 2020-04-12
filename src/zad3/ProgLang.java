1110ae450e197b08db9b12f499b7c57f37eaa121
Mateusz Majewski <majewski.mateusz000@gmail.com>
2020-04-12 13:54:08 +0200

    getProgsMap() working

--- src/zad3/ProgLang.java
+++ src/zad3/ProgLang.java
@@ -9,35 +9,33 @@ package zad3;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ProgLang {
     
-    Map<String, ArrayList<String>> mapAll = new HashMap<>();
+    Map<String, ArrayList<String>> mapAll = new LinkedHashMap<>();
     
     Map<String, ArrayList<String>> getLangsMap(){
         return mapAll;
-        // retMap = <nazwaJezyka, programisci tego jezyka ArrayList>
     }
     Map<String, ArrayList<String>> getProgsMap(){
-        System.out.println("***********************************");
-        Map<String, ArrayList<String>> retMap = new HashMap<>();
-        for(int i = 0; i < mapAll.size(); i++) {
-            ArrayList<String> programmers = new ArrayList<>();
-            ArrayList<String> langs = new ArrayList<>();
-            for(Map.Entry<String,ArrayList<String>> entry : mapAll.entrySet()){
-                programmers.addAll(entry.getValue());
-                langs.add(entry.getKey());
-                programmers.forEach(v -> {
-                    if(retMap.containsKey(v)){
-                        if(!retMap.get(v).contains(langs.get(langs.size()-1))){
-                            retMap.get(v).add(langs.get(langs.size()-1));
-                        }
-                    }else{
-                        ArrayList<String> prevValue = new ArrayList<>();
-                        prevValue.add(entry.getKey());
-                        retMap.put(v,prevValue);
-                    }
-                });
+        
+        Map<String, ArrayList<String>> retMap = new LinkedHashMap<>();
+        ArrayList<String> programmers = new ArrayList<>();
+        // adding programmers and languages into arrayLists:
+        for(HashMap.Entry<String,ArrayList<String>> entry : mapAll.entrySet()){
+            programmers.addAll(entry.getValue());
+        }
+        // making sure there are no duplicates:
+        programmers = programmers.stream().distinct().collect(Collectors.toCollection(ArrayList::new));
+        
+        for(String programmer : programmers) {
+            // filling return Map with programmers and empty Lists
+            retMap.put(programmer, new ArrayList<>());
+            
+            for(Map.Entry<String,ArrayList<String>> mapEntry : mapAll.entrySet()){
+                if(mapEntry.getValue().contains(programmer))
+                    retMap.get(programmer).add(mapEntry.getKey());
             }
         }
         return retMap;
@@ -52,7 +50,7 @@ public class ProgLang {
             while(sc.hasNextLine())
                 list.add(sc.nextLine());
             
-            Map<String,ArrayList<String>> plMap = new HashMap<>();
+            Map<String,ArrayList<String>> plMap = new LinkedHashMap<>();
             for(String s : list){
                 String[] arr = s.split("\t");
                 ArrayList<String> programmers = new ArrayList<>(Arrays.asList(arr).subList(1, arr.length));
